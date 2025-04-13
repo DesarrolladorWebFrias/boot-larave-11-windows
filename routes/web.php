bd040f7eb357237a8a0236c08b3eeb06d5f670d4
Luis Andres Lopez Frias <lalopezf@liconsa.gob.mx>
2025-04-12 18:35:42 -0600

    RUTAS AGRUPADAS

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard\PostController;
 use App\Http\Controllers\Dashboard\CategoryController;
 use App\Models\Category;
-
+use Illuminate\Routing\RouteGroup;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-Route::resource('category', CategoryController::class);
+
+
+Route::group(['prefix' => 'dashboard'] , function(){
+    Route::resource('post', PostController::class);
+    Route::resource('category', CategoryController::class);
+
+});
